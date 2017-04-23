3f85c37d653f767a011a81fb7dc2ef01513b72af
Aman Wolde <aman.wolde@valpo.edu>
2017-04-23 04:59:34 +0000

    add linting tools and fix some lints

--- flaskui/run.py
+++ flaskui/run.py
@@ -1,4 +1,4 @@
-#!/home/awolde/venv/bin/python
+"""This script is run to test the app in development mode """
+
 from app import app
 app.run(host='0.0.0.0', port=8080, debug=True)
-#app.run(debug=True)
--- flaskui/wsgi.py
+++ flaskui/wsgi.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+"""This script runs together with gunicorn to server the web app"""
+
 from app import app
 
 if __name__ == "__main__":
