4dda5125cde4e3096e11bbc86b025ee3dcd3e38e
erniemcb <erin.mcbride@valpo.edu>
2017-05-11 19:33:22 -0500

    Update shutdownec2.sh

--- bash-scripts/shutdownec2.sh
+++ bash-scripts/shutdownec2.sh
@@ -12,7 +12,9 @@ do
   runtime=$(( $(date "+%s")-$ltu )) #the time the instance run
   if [ $(($runtime/3600)) -gt $HOUR ]; then 
     echo "$id was running since $lt ...needs to be terminated"
-    aws ec2 terminate-instances --instance-ids $id
+    for x in $(cat CSVFILE)
+      do
+        terminate aws ec2 terminate-instances $id
   else
     echo "$id is safe"
   fi
